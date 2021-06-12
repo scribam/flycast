bce19e3d713e6decf34eaef0c7561826e7b1474c
scribam <scribam@users.noreply.github.com>
2021-06-12 21:19:54 +0200

    external: add ELFIO submodule
    replace libelf

--- core/reios/reios_elf.cpp
+++ core/reios/reios_elf.cpp
@@ -1,59 +1,43 @@
 #include "reios.h"
-
-#include "deps/libelf/elf.h"
-
 #include "hw/sh4/sh4_mem.h"
 
-bool reios_loadElf(const std::string& elf) {
+#include <elfio/elfio.hpp>
+#include <nowide/fstream.hpp>
 
-	FILE* f = nowide::fopen(elf.c_str(), "rb");
-	if (!f)
-		return false;
-
-	std::fseek(f, 0, SEEK_END);
-	size_t size = std::ftell(f);
-
-	if (size > 16 * 1024 * 1024) {
-		std::fclose(f);
+bool reios_loadElf(const std::string& elf)
+{
+	nowide::ifstream ifs(elf, std::ios::in | std::ios::binary);
+	if (!ifs) {
 		return false;
 	}
 
-	void* elfFile = malloc(size);
-	memset(elfFile, 0, size);
-
-	std::fseek(f, 0, SEEK_SET);
-	size_t nread = std::fread(elfFile, 1, size, f);
-	std::fclose(f);
-
-	if (nread != size || elf_checkFile(elfFile) != 0)
-	{
-		free(elfFile);
+	ifs.seekg(0, std::ios::end);
+	if (ifs.tellg() > 16 * 1024 * 1024) {
 		return false;
 	}
 
-	bool phys = false;
-	for (int i = 0; i < elf_getNumProgramHeaders(elfFile); i++)
-	{
-		// Load that section
-		uint64_t dest;
-		if (phys)
-			dest = elf_getProgramHeaderPaddr(elfFile, i);
-		else
-			dest = elf_getProgramHeaderVaddr(elfFile, i);
-		size_t len = elf_getProgramHeaderFileSize(elfFile, i);
-		void *src = (u8 *)elfFile + elf_getProgramHeaderOffset(elfFile, i);
+	ifs.seekg(0, std::ios::beg);
+
+	ELFIO::elfio reader;
+	reader.load(ifs);
+
+	ELFIO::Elf_Half seg_num = reader.segments.size();
+	for (int i = 0; i < seg_num; i++) {
+		const ELFIO::segment* pseg = reader.segments[i];
+
+		ELFIO::Elf64_Addr dest = pseg->get_virtual_address();
+		ELFIO::Elf_Xword len = pseg->get_file_size();
+
 		u8* ptr = GetMemPtr(dest, len);
-		if (ptr == NULL)
-		{
+		if (ptr == NULL) {
 			WARN_LOG(REIOS, "Invalid load address for section %d: %08lx", i, dest);
 			continue;
 		}
 		DEBUG_LOG(REIOS, "Loading section %d to %08lx - %08lx", i, dest, dest + len - 1);
-		memcpy(ptr, src, len);
+		memcpy(ptr, pseg->get_data(), len);
 		ptr += len;
-		memset(ptr, 0, elf_getProgramHeaderMemorySize(elfFile, i) - len);
+		memset(ptr, 0, pseg->get_memory_size() - len);
 	}
-	free(elfFile);
 
 	return true;
 }
