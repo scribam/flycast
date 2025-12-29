5409d3e86a1ded5245df67811871345dad04b57d
scribam <scribam@users.noreply.github.com>
2025-12-29 21:35:52 +0100

    external: add imgui submodule

--- core/ui/gui_util.h
+++ core/ui/gui_util.h
@@ -39,6 +39,11 @@ typedef bool (*StringCallback)(bool cancelled, std::string selection);
 void select_file_popup(const char *prompt, StringCallback callback,
 		bool selectFile = false, const std::string& extension = "");
 
+enum ImGuiWindowCustomFlags
+{
+	ImGuiWindowFlags_DragScrolling = 1 << 21,  // Allow scrolling by dragging content
+};
+
 void scrollWhenDraggingOnVoid(ImGuiMouseButton mouse_button = ImGuiMouseButton_Left);
 
 IMGUI_API const ImWchar*    GetGlyphRangesChineseSimplifiedOfficial();// Default + Half-Width + Japanese Hiragana/Katakana + set of 7800 CJK Unified Ideographs from General Standard Chinese Characters
