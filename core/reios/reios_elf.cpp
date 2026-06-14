723dfd004c2718f804c39de90b742a7eafcbcab4
scribam <scribam@users.noreply.github.com>
2026-06-14 21:00:16 +0200

    external: add ELFIO submodule

--- core/reios/reios_elf.cpp
+++ core/reios/reios_elf.cpp
@@ -1,56 +1,48 @@
 #include "reios.h"
-
-extern "C" {
-#include <elf/elf.h>
-}
-
 #include "hw/sh4/sh4_mem.h"
 
-bool reios_loadElf(const std::string& elf) {
+#include <fstream>
 
-	FILE* f = nowide::fopen(elf.c_str(), "rb");
-	if (!f)
+#include <elfio/elfio.hpp>
+
+bool reios_loadElf(const std::string& elf)
+{
+	/**
+	 * TODO: use nowide::fstream and fix the following errors
+	 * nowide.lib(cstdio.obj) : error LNK2005: "struct _iobuf * __cdecl nowide::fopen(char const *,char const *)" (?fopen@nowide@@YAPEAU_iobuf@@PEBD0@Z) already defined in winmain.obj [D:\a\flycast\flycast\build\flycast.vcxproj]
+     * nowide.lib(cstdio.obj) : error LNK2005: "int __cdecl nowide::remove(char const *)" (?remove@nowide@@YAHPEBD@Z) already defined in winmain.obj [D:\a\flycast\flycast\build\flycast.vcxproj]
+     * D:\a\flycast\flycast\build\Release\flycast.exe : fatal error LNK1169: one or more multiply defined symbols found [D:\a\flycast\flycast\build\flycast.vcxproj]
+	 */
+	std::ifstream ifs(elf, std::ios::in | std::ios::binary);
+	if (!ifs) {
 		return false;
+	}
 
-	std::fseek(f, 0, SEEK_END);
-	size_t size = std::ftell(f);
+	ifs.seekg(0, std::ios::end);
+	std::size_t size = ifs.tellg();
 
 	if (size == 0 || size > 16_MB) {
-		std::fclose(f);
 		return false;
 	}
 
-	void* elfF = malloc(size);
+	ifs.seekg(0, std::ios::beg);
 
-	std::fseek(f, 0, SEEK_SET);
-	size_t nread = std::fread(elfF, 1, size, f);
-	std::fclose(f);
+	ELFIO::elfio reader;
+	reader.load(ifs);
+	if (reader.get_machine() != ELFIO::EM_SH)
+		WARN_LOG(REIOS, "Elf file is not for Hitachi SH: machine %d", reader.get_machine());
 
-	elf_t elfFile;
-	if (nread != size || elf_newFile(elfF, nread, &elfFile) != 0)
-	{
-		free(elfF);
-		return false;
-	}
-
-	Elf32_Ehdr const *header = (const Elf32_Ehdr *)elfFile.elfFile;
-	if (header->e_machine != EM_SH)
-		WARN_LOG(REIOS, "Elf file is not for Hitachi SH: machine %d", header->e_machine);
-
-	for (size_t i = 0; i < elf_getNumProgramHeaders(&elfFile); i++)
-	{
-		uint32_t type = elf_getProgramHeaderType(&elfFile, i);
-		if (type != PT_LOAD) {
-			DEBUG_LOG(REIOS, "Ignoring section %d type %d", (int)i, type);
+	for (const auto &segment : reader.segments) {
+		if (segment->get_type() != ELFIO::PT_LOAD) {
+			DEBUG_LOG(REIOS, "Ignoring section %d type %d", segment->get_index(), segment->get_type());
 			continue;
 		}
 		// Load/initialize that section
-		uint64_t dest = elf_getProgramHeaderVaddr(&elfFile, i);
-		size_t len = elf_getProgramHeaderFileSize(&elfFile, i);
-		void *src = (u8 *)(elfFile.elfFile) + elf_getProgramHeaderOffset(&elfFile, i);
-		size_t memsize = elf_getProgramHeaderMemorySize(&elfFile, i);
+		ELFIO::Elf64_Addr dest = segment->get_virtual_address();
+		ELFIO::Elf_Xword len = segment->get_file_size();
+		ELFIO::Elf_Xword memsize = segment->get_memory_size();
 		if (memsize < len) {
-			WARN_LOG(REIOS, "Invalid memory size for section %d: %lx", (int)i, (long)memsize);
+			WARN_LOG(REIOS, "Invalid memory size for section %d: %lx", segment->get_index(), (long)memsize);
 			continue;
 		}
 		if (memsize == 0)
@@ -58,14 +50,13 @@ bool reios_loadElf(const std::string& elf) {
 		u8* ptr = GetMemPtr(dest, memsize);
 		if (ptr == nullptr)
 		{
-			WARN_LOG(REIOS, "Invalid load address or size for section %d: %08lx size %lx", (int)i, (long)dest, (long)memsize);
+			WARN_LOG(REIOS, "Invalid load address or size for section %d: %08lx size %lx", (int)segment->get_index(), (long)dest, (long)memsize);
 			continue;
 		}
-		DEBUG_LOG(REIOS, "Loading section %d to %08lx - %08lx", (int)i, (long)dest, (long)(dest + memsize - 1));
-		memcpy(ptr, src, len);
+		DEBUG_LOG(REIOS, "Loading section %d to %08lx - %08lx", segment->get_index(), (long)dest, (long)(dest + memsize - 1));
+		memcpy(ptr, segment->get_data(), len);
 		memset(ptr + len, 0, memsize - len);
 	}
-	free(elfF);
 
 	return true;
 }
